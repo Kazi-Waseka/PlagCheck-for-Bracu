db23c9daa420a06c012288b4d6b13bc3783a96aa
LENOVO <LENOVO@LAPTOP-TO9OL4VP>
2023-04-27 20:54:39 +0600

    Add project files.

---
+++ WebApplication2/Models/Domain/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace WebApplication2.Models.Domain
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string Name {get; set;}
+
+        public string ? ProfilePicture { get; set; }
+
+
+
+    }
+}
